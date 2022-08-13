35708f9800c3b7f5cf82c8ad13568ce2eea2895d
musicboy17 <pratham@yandex.ru>
2022-08-13 21:15:32 +0300

    Задача N21

--- Exxes002/Program.cs
+++ Exxes002/Program.cs
@@ -4,23 +4,25 @@
 // A (7,-5, 0); B (1,-1,9) -> 11.53
 
 Console.Write("Введите координату X первой точки: ");
-int x1 = Convert.ToInt(Console.ReadLine());
+int x1 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите координату Y первой точки: ");
-int y1 = Convert.ToInt(Console.ReadLine());
+int y1 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите координату Z первой точки: ");
-int z1 = Convert.ToInt(Console.ReadLine());
+int z1 = Convert.ToInt32(Console.ReadLine());
 
 
 Console.Write("Введите координату X второй точки: ");
-int x2 = Convert.ToInt(Console.ReadLine());
+int x2 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите координату Y вторвой точки: ");
-int y2 = Convert.ToInt(Console.ReadLine());
+int y2 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите координату Z второй точки: ");
-int z2 = Convert.ToInt(Console.ReadLine());
+int z2 = Convert.ToInt32(Console.ReadLine());
 
 double result = Math.Sqrt(Math.Pow(x1 - x2,2) + (y1 - y2,2) + (z1 - z2,2));
 
 Console.WriteLine($"Расстояние равно: {Math.Round(result,2)}");
+
+
 
 
 
